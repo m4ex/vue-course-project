6b1a7d3ea546345e160026286550b86eab277a74
m4ex <4exmail@gmail.com>
2023-09-04 20:30:12 +0300

    fix onAuthenticatedHandler

--- src/api/httpClient/httpClient.js
+++ src/api/httpClient/httpClient.js
@@ -33,7 +33,9 @@ httpClient.interceptors.response.use(
   (error) => {
     if ((!error.response || error.code === 'ECONNABORTED') && httpClient._onNetworkError) {
       httpClient._onNetworkError();
-    } else if (error.response.statusCode === 401 && httpClient._onAuthenticatedHandler) {
+      //COMMENT в строки ниже было error.response.statusCode вместо error.response.status результат не с кодом 401 не оборачивается в контейнер
+      // Да и вообще с этим контейнером результата как-то не удобно, он больше путал чем помогал
+    } else if (error.response.status === 401 && httpClient._onAuthenticatedHandler) {
       httpClient._onAuthenticatedHandler();
     } else {
       return Promise.reject(error);
